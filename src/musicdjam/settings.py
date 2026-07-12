5c1928e96f146e3ff5d9bf6832c2c29dda4d9f07
LukaLominadze <l.lominadze2927@gmail.com>
2026-07-12 19:44:00 +0400

    fixing i18n configuration

--- src/musicdjam/settings.py
+++ src/musicdjam/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'frontend.context_processors.profile_picture',
