862183eb369f059945d8f05fc3b76430b6792932
LukaLominadze <l.lominadze2927@gmail.com>
2026-07-12 19:31:54 +0400

    removing unnecessary inclusion of i18n package

--- src/musicdjam/settings.py
+++ src/musicdjam/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.i18n',
     'rest_framework',
     'rest_framework_simplejwt',
     'api',
